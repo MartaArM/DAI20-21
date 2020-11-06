44bb1d9efbc9c3ec2abae24e48332293a07e27f2
MartaArM <martilla311@gmail.com>
2020-11-06 11:05:16 +0100

    Empezando base de datos

--- Pr3/app/app.py
+++ Pr3/app/app.py
@@ -1,11 +1,64 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, session, request;
+from flask_sqlalchemy import SQLAlchemy;
+from static import database;
 
 app = Flask(__name__);
+app.secret_key = "&LVKaB}fK(EqR";
+
+
 
 @app.route('/')
 def pagina_inicio():
-	return render_template('inicio.html')
+	if session['logged_in'] == True:
+		return render_template('inicio.html', login='true');
+	else:
+		return render_template('inicio.html', login='false');
+
+	
 
 @app.route('/prueba')
 def pagina_prueba():
 	return render_template('pagina_prueba.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+	password = request.form['password'];
+	username = request.form['username'];
+
+	users = User.query.all();
+
+	for user in users:
+		
+	if password == '12345' and username == 'marta':
+		session['logged_in'] = True;
+		return render_template('inicio.html', login='true', nombre_us=username);
+	else:
+		return render_template('inicio.html', login='false');
+	
+@app.route('/unlogin', methods=["GET", "POST"])
+def unlogin():
+	password = "";
+	user = "";
+	session['logged_in'] = False;
+	return render_template('inicio.html', login='false');
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+	password = "";
+	user = "";
+	session['logged_in'] = False;
+	return render_template('registro_usuario.html', login='false');
+
+@app.route('/register_data', methods=["GET", "POST"])
+def register_data():
+
+	password = request.form['password'];
+	username = request.form['username'];
+
+	user = User(username=username, password=password);
+	db.session.add(user);
+	db.session.commit();
+	return pepe;
+	# return render_template('registro_usuario.html', login='false');
+
+
---
+++ Pr3/app/static/database.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy;
+from flask import Flask;
+
+app = Flask(__name__);
+db_name = 'users.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name;
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True;
+db = SQLAlchemy(app);
+
+# Database
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(30), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
