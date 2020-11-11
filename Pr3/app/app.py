08fbc64b15e23ecd9f48b1e06971c0e2379eb968
MartaArM <martilla311@gmail.com>
2020-11-11 18:45:42 +0100

    Interfaz de ejercicio 1 terminada

--- Pr3/app/app.py
+++ Pr3/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, session, request, redirect, url_for;
 from flask_sqlalchemy import SQLAlchemy;
 import config
 from models import Users, db
+import random
 
 app = Flask(__name__);
 app.config.from_object(config)
@@ -95,6 +96,7 @@ def register_data():
 
 @app.route('/ejercicio1', methods=["GET", "POST"])
 def ejercicio1():
+	session['num'] = random.randrange(101);
 	if 'logged_in' in session:
 		if session['logged_in'] == True:
 			return render_template('ejercicio1.html', login='true', nombre_us=session['user_login']);
@@ -102,4 +104,25 @@ def ejercicio1():
 			return render_template('ejercicio1.html', login='false');
 	else:
 		return render_template('ejercicio1.html', login='false');
+
+@app.route('/resolver_ej1', methods=["GET", "POST"])
+def resolver_ej1():
+	valor = request.form['valor'];
+
+	opcion = ""
+	if int(valor) <  session['num']:
+		opcion = "menor"
+	elif int(valor) > session['num']:
+		opcion = "mayor"
+	else:
+		opcion = "igual"
+
+	if 'logged_in' in session:
+		if session['logged_in'] == True:
+			return render_template('ejercicio1.html', login='true', nombre_us=session['user_login'], opcion=opcion);
+		else:
+			return render_template('ejercicio1.html', login='false', opcion=opcion);
+	else:
+		return render_template('ejercicio1.html', login='false', opcion=opcion);
+
 
