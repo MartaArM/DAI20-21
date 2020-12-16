2a41fa798c7f279baf92fbbacfac8ac9923a43a1
MartaArM <martilla311@gmail.com>
2020-12-16 19:09:47 +0100

    Practica 7 casi terminada

---
+++ Pr7/miapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('test_template', views.test_template, name='test_template'),
+  path('libros/add', views.add_libros, name='add_libros'),
+  path('libros/see', views.see_libros, name='see_libros'),
+  path('libros/edit', views.edit_libros, name='edit_libros'),
+  path('libros/edit_form', views.edit_libros_form, name='edit_libros_form'),
+  path('libros/edit_libro_ok', views.edit_libros_ok, name='edit_libros_ok'),
+  path('prestamo/see', views.see_prestamos, name='see_prestamos'),
+  path('prestamos/add', views.add_prestamos, name='add_prestamos'),
+  path('prestamos/edit', views.edit_prestamos, name='edit_prestamos'),
+  path('prestamos/edit_form', views.edit_prestamos_form, name='edit_prestamos_form'),
+  path('prestamos/edit_prestamo_ok', views.edit_prestamos_ok, name='edit_prestamos_ok'),
+]
